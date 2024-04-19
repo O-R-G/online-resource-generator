c13bcf4c249738cc267f46722fea61ee6b3c6955
wei <wei@o-r-g.com>
2024-04-19 13:08:16 +0800

    update paths in webpack.config.js

--- static/js/webpack-sample.config.js
+++ static/js/webpack-sample.config.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './static/js/src/index.js',
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'static/js/dist'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
   },
 }
