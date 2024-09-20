5d4999a9f9f7bd25a6ed6556f5494d6e9a15e286
wei <wei@o-r-g.com>
2024-09-20 10:11:05 +0800

    rename FontLoader to avoid conflict

---
+++ static/js/src/ORGFontLoader.js
@@ -0,0 +1,44 @@
+// import { FontLoader as threeFontLoader } from 'three/addons/loaders/FontLoader.js';
+
+export class ORGFontLoader {
+	constructor(fonts_data){
+        this.fonts_data = fonts_data;
+        this.loaded_css = [];
+        
+        // this.styles = fonts_data['styles-to-preload'];
+        // this.stylesheets = fonts_data['stylesheets'];
+        // this.files = fonts_data['files'];
+        this.init();
+	}
+    init(cb){
+        for(const key in this.fonts_data) {
+            this.addStylesheet(this.fonts_data[key]['stylesheet']);
+            this.addDummyDiv(this.fonts_data[key]['style']);
+        }
+        if (typeof cb === 'function') cb();
+    }
+    addStylesheet(path){
+        console.log('eee');
+        if(this.loaded_css.includes(path)) return;
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = path;
+        document.head.appendChild(link);
+        this.loaded_css.push(path)
+    }
+    addDummyDiv(style){
+        let style_str = '';
+        for(let prop in style) {
+            console.log(prop);
+            if(!style[prop]) continue;
+            style_str += `font-${prop}: ${style[prop]}; `;
+        }
+        let temp = document.createElement('div');
+        temp.innerHTML = '&nbsp;';
+        console.log(style_str);
+        temp.style = style_str;
+        document.body.appendChild(temp);
+    }
+}
+const fontLoaderInstance = new ORGFontLoader(fonts, '');
+export default fontLoaderInstance;
