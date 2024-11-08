b18ee5548dfeed82b65bc64ca3e0ef7882be670c
wei <wei@o-r-g.com>
2024-11-08 13:43:19 +0800

    update gitignore and webpacl-sample.config.js

--- static/js/webpack-sample.config.js
+++ static/js/webpack-sample.config.js
@@ -3,8 +3,15 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  // entry: {
+  //   'index': './src/index.js',
+  //   'shape-animated': './src/ShapeAnimated.js',
+  //   'shape-static': './src/ShapeStatic.js',
+  //   'canvas': './src/Canvas.js',
+  // },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
+    // filename: '[name].bundle.js',
   },
 }
